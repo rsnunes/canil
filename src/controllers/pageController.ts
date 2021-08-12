0c9118e9e1880674f83267f6c00c38df2b614d31
Renato Nunes <r.silveira.nunes14@gmail.com>
2021-08-12 20:14:54 -0300

    views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response)=>{
-    res.send('home');
+    res.render('pages/page');
 };
 export const dogs = (req: Request, res: Response)=>{
 
